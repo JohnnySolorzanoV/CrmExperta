939647a81e10f39f0858a59c1680b0e93894af72
Jonnathan Solorzano Vera <johnnyjsolver99@gmail.com>
2026-05-19 00:35:36 -0500

    fix: corregir clase de alineación de texto en varios componentes

--- frontend/crmExperta/src/components/frontendCRM.vue
+++ frontend/crmExperta/src/components/frontendCRM.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="container text center">
+  <div class="container text-center">
     <h1>EXPERTA&ABOGADOS</h1>
     <h7>Firma legal con especialistas en cada área jurídica</h7>
   </div>
--- frontend/crmExperta/src/components/loginPerfil.vue
+++ frontend/crmExperta/src/components/loginPerfil.vue
@@ -3,7 +3,7 @@
 </script>
 //debo hacerlo funcionar con el boton de Inicio de sesion de Inicio CRM
 <template>
-  <div class="container text center">
+  <div class="container text-center">
     <h1>EXPERTA&ABOGADOS</h1>
     <h7>Firma legal con especialistas en cada área jurídica</h7>
   </div>
